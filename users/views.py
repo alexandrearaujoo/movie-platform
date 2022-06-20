b38ec0f5b831320b0e82414b9bb8ab5b044f1400
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-20 12:09:12 -0300

    feat: UserView done

--- users/views.py
+++ users/views.py
@@ -1,1 +1,16 @@
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from users.serializers import UserSerializer
+
+
+class UserView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return Response(serializer.data, status.HTTP_201_CREATED)
