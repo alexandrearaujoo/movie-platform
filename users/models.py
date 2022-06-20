cf87a2bffdc9a3a4fadf92a78269188f5912ceb1
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-20 11:54:56 -0300

    fix: add fist_name and last_name in required_fields list

--- users/models.py
+++ users/models.py
@@ -9,6 +9,6 @@ class User(AbstractUser):
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     objects = CustomUserManager()
