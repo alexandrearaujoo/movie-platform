fa45af5d4329bdb5369d667a13ee55d6d6910da2
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-20 12:15:02 -0300

    fix: fixing import and path url

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
-from models import User
+from .models import User
 
 class UserSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_onlyd=True)
+    id = serializers.IntegerField(read_only=True)
     email = serializers.EmailField()
     first_name = serializers.CharField(max_length=50)
     password = serializers.CharField(max_length=255)
