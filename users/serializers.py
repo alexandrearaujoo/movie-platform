e2ec42c576fc1fa5091b8a3629ac7828f0924eda
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-20 12:06:57 -0300

    feat: UserSerializer done

---
+++ users/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from models import User
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_onlyd=True)
+    email = serializers.EmailField()
+    first_name = serializers.CharField(max_length=50)
+    password = serializers.CharField(max_length=255)
+    last = serializers.CharField(max_length=50)
+    date_joined = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
+
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
