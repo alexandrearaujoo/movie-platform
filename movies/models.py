f4d2834dda1045caab90771f34b1b479615c06c2
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-21 12:31:08 -0300

    feat: model movie done

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=127)
+    duration = models.CharField(max_length=10)
+    premiere = models.DateField()
+    classification = models.IntegerField()
+    synopsis = models.TextField()
