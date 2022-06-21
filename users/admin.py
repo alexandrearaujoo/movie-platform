ac458cc74ce21b3619f98150aa3ed2176a1534d1
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-21 14:21:21 -0300

    fix: changing models in django admin

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from genres.models import Genre
-from movies.models import Movie
-from reviews.models import Review
 
 from users.models import User
 
 admin.site.register(User)
-admin.site.register(Review)
-admin.site.register(Movie)
-admin.site.register(Genre)
+
