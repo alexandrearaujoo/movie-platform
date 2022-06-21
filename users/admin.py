fac524263b8d280feb9eed156631271de49ba06a
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-21 13:38:26 -0300

    feat: add models in django admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from genres.models import Genre
+from movies.models import Movie
+from reviews.models import Review
 
-# Register your models here.
+from users.models import User
+
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Movie)
+admin.site.register(Genre)
