bae90e39fa76b93f4173a88727d17e6678700290
Alexandre Araujo <alexandre01393@gmail.com>
2022-06-23 11:04:50 -0300

    feat: choice fields add

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
+class CategoryRecomendation(models.TextChoices):
+    MUST_WATCH = ('must watch',)
+    SHOULD_WATCH = ('should watch',)
+    AVOID_WATCH = ('avoid watch',)
+    NO_OPINION = ('no opinion',)
+
 class Review(models.Model):
     starts = models.IntegerField()
     review = models.TextField()
     spoilers = models.BooleanField(default=False)
-    recomenadation = models.CharField(max_length=50)
+    recomenadation = models.CharField(
+        max_length=50, 
+        choices=CategoryRecomendation.choices, 
+        default=CategoryRecomendation.NO_OPINION
+    )
 
     user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='users')
 
